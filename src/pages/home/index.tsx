a48fcf24c249b2abf209acdec3a1bb89df680b0e
Andrey Airson <andreyairson@gmail.com>
2023-02-21 20:44:33 -0300

    chore/refactor: added swipper and changed carrousel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,53 +1,37 @@
-import { FC, useContext, useEffect } from 'react'
+import { FC, useContext } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { LogoNlw } from '@/assets'
 import { CreateAdBanner, CreateAdModal, GameBanner } from '@/components'
-import axios from 'axios'
-import { NLWContext } from '../../context'
-import { useKeenSlider } from 'keen-slider/react'
-import 'keen-slider/keen-slider.min.css'
+import { NLWContext } from '@/context'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 const Home: FC = () => {
-  const { games, getAllGamesFromAPI } = useContext(NLWContext)
-
-  const [ref] = useKeenSlider<HTMLDivElement>({
-    loop: true,
-    mode: 'free-snap',
-    slides: {
-      perView: 6,
-      spacing: 10
-    }
-  })
-
-  useEffect(() => {
-    axios('http://localhost:3333/games').then(({ data }) => {
-      getAllGamesFromAPI(data)
-    })
-  }, [games])
+  const { games } = useContext(NLWContext)
 
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20 '>
       <LogoNlw />
-      <h1 className='text-6xl text-white font-black mt-20'>
+      <h1 className='text-6xl text-white font-black pb-20 mt-10'>
         Seu <span className='bg-nlw-gradient text-transparent bg-clip-text'>duo</span> está aqui.
       </h1>
 
-      <div {...{ ref }} className='keen-slider grid grid-cols-6 mt-16'>
+      <Swiper className='mySwiper max-w-[1344px] z-[1]' spaceBetween={10} slidesPerView={6}>
         {games &&
           games.map(game => {
             return (
-              <div key={game.id} className='h-[282px] w-[250px] keen-slider__slide'>
+              <SwiperSlide key={game.id}>
                 <GameBanner
-                  className='relative overflow-hidden cursor-pointer'
+                  className='relative overflow-hidden cursor-pointer w-[250] '
                   addsCount={game._count.ads}
                   bannerUrl={game.bannerUrl}
                   title={game.title}
                   id={game.id}
                 />
-              </div>
+              </SwiperSlide>
             )
           })}
-      </div>
+      </Swiper>
 
       <Dialog.Root>
         <CreateAdBanner />
